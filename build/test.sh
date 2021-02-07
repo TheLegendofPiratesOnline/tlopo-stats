a096e3bcc41a459effbf8214df63380cbeec5783
loblao <12ksit@gmail.com>
2021-02-07 01:51:45 -0300

    Docker: No -it when running tests

--- build/test.sh
+++ build/test.sh
@@ -2,4 +2,4 @@
 
 rm -f ./tlopostats
 docker image build -t tlopostats -f Dockerfile ..
-docker run --rm -it --net host -entrypoint=/bin/sh tlopostats /app/build/entrypoint_test.sh $1 $2
+docker run --rm --net host -entrypoint=/bin/sh tlopostats /app/build/entrypoint_test.sh $1 $2
